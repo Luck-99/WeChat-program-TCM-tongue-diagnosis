f9b4299654f042f15e70969578be6470079872da
Luck-99 <1427703250@qq.com>
2020-04-23 14:03:24 +0800

    1

--- pages/home/index.js
+++ pages/home/index.js
@@ -9,7 +9,25 @@ Page({
         username: '',
         contents: "18500860320",
         loginshow:true,
-        time:new Date
+        i: 0,
+        imgsrc: "",
+        imgsrc0: "",
+        imgsrc1: "",
+        imgsrc2: "",
+        latitude: 0,
+        longitude: 0,
+        restype: [ "", "", "" ],
+        user_cuttongue: "",
+        cha1: "",
+        cha2: "",
+        cha3: "",
+        cha4: "",
+        cha5: "",
+        cha6: "",
+        cha7: "",
+        cha8: "",
+        cha9: "",
+        cha10: ""
     },
     onLoad: function(t) {
       var s=this;
@@ -47,6 +65,7 @@ Page({
         });
     },
     sharefriends:function(){
+      var o = this;
       var restime= new Date().toJSON().substring(0, 10).replace(/-/g,'') + new Date().toTimeString().substring(0,8).replace(/:/g,'');
       wx.request({
         url: "http://www.bayescience.com/api/analysis",
@@ -63,10 +82,11 @@ Page({
       },
         method: "POST",
         success: function(a) {
-          console.log(a);
-          /*if (console.log("识别的结果："), console.log(a), console.log(a.data.code), 88 == a.data.code) {
+          /*console.log(a);
+          console.log(a.data);*/
+          if (console.log("识别的结果："), console.log(a), console.log(a.data.code), 88 == a.data.code) {
               var n;
-              o.setData((n = {}, t(n, "restype[0]", a.data.tizhi), t(n, "imgsrc", e), t(n, "display1", "block"), 
+              o.setData((n = {}, t(n, "restype[0]", a.data.tizhi), t(n, "imgsrc", e), 
               t(n, "display2", "none"),  t(n, "user_cuttongue", a.data.cutTongue), 
               t(n, "cha1", a.data.cha1), t(n, "cha2", a.data.cha2), t(n, "cha3", a.data.cha3), 
               t(n, "cha4", a.data.cha4), t(n, "cha5", a.data.cha5), t(n, "cha6", a.data.cha6), 
@@ -88,12 +108,43 @@ Page({
               title: "请求超时",
               icon: "success",
               duration: 1500
-          });*/
+          });
       },
       fail: function(t) {
           wx.hideLoading(), wx.showToast({
               title: "请求超时",
               duration: 1500
+          });
+      }
+      });
+      //console.log(this.data.cha1);
+      wx.request({
+        url: 'https://120.26.172.111/test/Receive_data.php',
+        data: {
+          /*openid: getApp().globalData.openID,
+          latitude: e.data.latitude,
+          longitude: e.data.longitude,
+          result: t,*/
+          user_cuttongue: this.data.user_cuttongue,
+          user_cha1: this.data.cha1,
+          user_cha2: this.data.cha2,
+          user_cha3: this.data.cha3,
+          user_cha4: this.data.cha4,
+          user_cha5: this.data.cha5,
+          user_cha6: this.data.cha6,
+          user_cha7: this.data.cha7,
+          user_cha8: this.data.cha8,
+          user_cha9: this.data.cha9,
+          user_cha10: this.data.cha10
+      },
+      header: {
+          "Content-Type": "application/json"
+      },
+      method: "GET",
+      success: function(a) {
+          console.log("插入数据的结果："), console.log(a), "unsuccess" != a.data && wx.setStorageSync("tjtime", a.data), 
+          wx.redirectTo({
+              url: "../result/index?tizhi=" + t
           });
       }
       });
